d3ebb1f4d02b40ebdcd0ff540b6424654a302228
wldn143 <wldn143@gmail.com>
2022-02-21 21:51:58 +0900

    Dark Mode

--- src/apollo.js
+++ src/apollo.js
@@ -1,9 +1,8 @@
 import {ApolloClient, InMemoryCache, makeVar} from "@apollo/client";
 
 const TOKEN = "token";
-
+const DARK_MODE = "DARK_MODE"
 export const isLoggedInVar = makeVar(Boolean(localStorage.getItem(TOKEN)));
-export const darkModeVar = makeVar(false);
 
 export const logUserIn = (token)=>{
     localStorage.setItem(TOKEN,token)
@@ -14,6 +13,18 @@ export const logUserOut = (history)=>{
     localStorage.removeItem(TOKEN)
     history?.replace();
     window.location.reload();
+}
+
+export const darkModeVar = makeVar(Boolean(localStorage.getItem(DARK_MODE)));
+
+export const enableDarkMode=()=>{
+    localStorage.setItem(DARK_MODE,"enable")
+    darkModeVar(true)
+}
+
+export const disableDarkMode=()=>{
+    localStorage.setItem(DARK_MODE,"disable")
+    darkModeVar(false);
 }
 
 export const client = new ApolloClient({
--- src/components/auth/AuthLayout.js
+++ src/components/auth/AuthLayout.js
@@ -1,4 +1,8 @@
+import { useReactiveVar } from "@apollo/client";
+import { faMoon, faSun } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
+import { darkModeVar, disableDarkMode, enableDarkMode } from "../../apollo";
 
 const Container = styled.div`
     display:flex;
@@ -14,10 +18,23 @@ const Wrapper =styled.div`
     width:100%;
 `;
 
+const Footer =styled.footer`
+  margin-top:20px;
+`
+const DarkModeBtn = styled.span`
+  cursor:pointer;
+`
+
 function AuthLayout({children}){
+  const darkMode = useReactiveVar(darkModeVar);
     return (
       <Container>
         <Wrapper>{children}</Wrapper>
+        <Footer>
+        <DarkModeBtn onClick={darkMode ? disableDarkMode : enableDarkMode}>
+          <FontAwesomeIcon icon={darkMode ? faSun : faMoon} />
+        </DarkModeBtn>
+      </Footer>
       </Container>
     );
 }
